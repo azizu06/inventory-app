6e6a327e8a8e141a9faaf1f9c9aa588d025d0b05
azizu06 <ab725492@ucf.edu>
2026-04-24 17:28:43 -0400

    Remove stale message board routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,4 @@ const { Router } = require("express");
 const router = Router();
 const controller = require("../controllers/index.js");
 
-router.get("/", controller.getMsgs);
-router.get("/new", controller.createMsgGet);
-router.post("/new", controller.createMsgPost);
-router.get("/msgs/:id", controller.getMsgDetail);
-
 module.exports = router;
